cf81d29597516f25d917faf66594f8ed4297fbf2
Dmitriy <dmed832@yandex.ru>
2023-01-04 14:07:31 +0300

    add PathHelper

---
+++ src/PathHelper.java
@@ -0,0 +1,20 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathHelper {
+
+    public static Path buildFileName(String path, String suffix) {
+        Path pathPrimary = Path.of(path);
+        Path directory = pathPrimary.getParent();
+        String fileName = pathPrimary.getFileName().toString();
+        String newFileName;
+        if (fileName.contains(".")) {
+            int indexOfDot = fileName.lastIndexOf(".");
+            newFileName = fileName.substring(0, indexOfDot) + suffix +
+                    fileName.substring(indexOfDot);
+        } else {
+            newFileName = fileName + suffix;
+        }
+        return directory.resolve(Path.of(newFileName));
+    }
+}
