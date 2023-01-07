2daf2ee12b2bbc93df6b07279e1d5b35fc7b2422
Dmitriy <dmed832@yandex.ru>
2023-01-07 21:36:23 +0300

    improved isValidateText

--- src/Bruteforce.java
+++ src/Bruteforce.java
@@ -1,43 +1,40 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Scanner;
+import java.util.*;
 
 public class Bruteforce {
 
     private final Scanner scanner = new Scanner(System.in);
     private final CesarCipher cesarCipher = new CesarCipher();
 
-    public void bruteforce() throws IOException {  // вместо запроса имени второго файла - добавлять суффикс.  C:\Downloads\1_encrypted.txt
+    public void bruteforce() throws IOException {  // C:\Downloads\1_encrypted.txt
         System.out.println("Введите путь к файлу для расшифровки:");
         String path = scanner.nextLine();
 
         Path newPath = PathHelper.buildFileName(path, "_bruteforced");
 
-        try (BufferedReader reader = Files.newBufferedReader(Paths.get(path));
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(path));
              BufferedWriter writer = Files.newBufferedWriter(newPath)) {
             StringBuilder builder = new StringBuilder();
-            reader.mark((int) Files.size(Paths.get(path)));
+            List<String> list = new ArrayList<>();
             while (reader.ready()) {
                 String string = reader.readLine();
                 builder.append(string);
+                list.add(string);
             }
 
-            for (int i = 1; i <= cesarCipher.alphabetLength(); i++) {
+            for (int i = 0; i < cesarCipher.alphabetLength(); i++) {
                 String decrypt = cesarCipher.decrypt(builder.toString(), i);
                 if (isValidateText(decrypt)) {
+
+                    for (String string : list) {
+                        String encrypt = cesarCipher.decrypt(string, i);
+                        writer.write(encrypt + System.lineSeparator());
+                    }
                     System.out.println("Текст успешно сохранен в файл " + newPath.getFileName());
                     System.out.println("Ключ расшифровки: " + i + System.lineSeparator());
-
-                    reader.reset();
-                    while (reader.ready()) {
-                        String string = reader.readLine();
-                        String encryptedDecrypted = cesarCipher.decrypt(string, i);
-                        writer.write(encryptedDecrypted + System.lineSeparator());
-                    }
+                    break;
                 }
             }
         }
@@ -45,30 +42,30 @@ public class Bruteforce {
 
     private boolean isValidateText(String text) {
 
-        String answer = "";
-
         boolean isValidate = false;
 
-        String[] strings = text.split(" ");
-        for (int i = 0; i < strings.length; i++) {
-            if (strings[i].length() > 28) {
+        for (String string : text.split(" ")) {
+            if (string.length() > 28) {
                 return false;
             }
         }
+        // проверить корректную работу при использовании метода matcher
         if (text.contains(". ") || text.contains(", ") || text.contains("! ") || text.contains("? ")) {
             isValidate = true;
         }
-        // написать запрос к пользователю на валидацию текста:  Текст валидный (Y/N)?  + проверка ввода
-        // если не валидный: isValidate = false
-        System.out.println(text.substring(0, Math.min(text.length(), 1000)));
-        System.out.println("Текст корректно расшифрован?");
-        do {
-            System.out.print("Если корректно - нажмите Y, если не корректно - нажмите N: ");
-            if (scanner.hasNextLine()) {
-                answer = scanner.nextLine();
-            }
-        } while (!answer.equalsIgnoreCase("y") && !answer.equalsIgnoreCase("n"));
 
-        return (isValidate && answer.equalsIgnoreCase("y"));
+        while (isValidate) {
+            System.out.println(text.substring(0, Math.min(text.length(), 1000)));
+            System.out.println("Текст корректно расшифрован? (Y/N)");
+            String answer = scanner.nextLine();
+            if (answer.equalsIgnoreCase("y")) {
+                return true;
+            } else if (answer.equalsIgnoreCase("n")) {
+                isValidate = false;
+            } else {
+                System.out.println("Выберите Y или N");
+            }
+        }
+        return false;
     }
 }
