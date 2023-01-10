6ac87885570a8e9badba77dfe7015410159f50e5
Dmitriy <dmed832@yandex.ru>
2023-01-10 23:18:13 +0300

    add mapToList

--- src/Parsing.java
+++ src/Parsing.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -6,26 +7,46 @@ import java.util.*;
 
 public class Parsing {
 
+    private final Scanner scanner = new Scanner(System.in);
+
+    private final Map<Character, Integer> encrypted = new HashMap<>();
+    private final Map<Character, Integer> open = new HashMap<>();
+    private final Map<Character, Character> decrypted = new HashMap<>();
+
     public void parser() throws IOException {
         System.out.println("Введите полный адрес зашифрованного файла");
-        Scanner scanner = new Scanner(System.in);
         String pathEncrypted = scanner.nextLine();
         System.out.println("Введите полный адрес файла с открытым текстом");
         String pathOpen = scanner.nextLine();
-        Map<Character, Integer> encrypted = new HashMap<>();
-        Map<Character, Integer> open = new HashMap<>();
-        Map<Character, Integer> fillEncrypted = fillMapValues(encrypted, pathEncrypted);
-        Map<Character, Integer> fillOpen = fillMapValues(open, pathOpen);
-        Set<Map.Entry<Character, Integer>> entries = fillEncrypted.entrySet();
-        List<Map.Entry<Character, Integer>> list = new ArrayList<>(entries);
-//        Collections.sort(list, );
+        Path newPath = PathHelper.buildFileName(pathEncrypted, "_parsing");
 
+        List<Map.Entry<Character, Integer>> listEncrypted = mapToList(fillMapValues(encrypted, pathEncrypted));
+        List<Map.Entry<Character, Integer>> listOpen = mapToList(fillMapValues(open, pathOpen));
 
+        if (listEncrypted.size() <= listOpen.size()) {
+            for (int i = 0; i < listEncrypted.size(); i++) {
+                decrypted.put(listEncrypted.get(i).getKey(), listOpen.get(i).getKey());
+            }
+        } else {
+            System.out.println("Размер файла статистики недостаточен, необходимо больше символов");
+        }
 
-
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(pathEncrypted));
+             BufferedWriter writer = Files.newBufferedWriter(newPath)) {
+            while (reader.ready()) {
+                StringBuilder builder = new StringBuilder();
+                String string = reader.readLine();
+                for (char encryptedChar : string.toCharArray()) {
+                    Character decryptedChar = decrypted.get(encryptedChar);
+                    builder.append(decryptedChar);
+                }
+                writer.write(builder + System.lineSeparator());
+            }
+        }
+        System.out.println("Текст успешно сохранен в файл " + newPath.getFileName() + System.lineSeparator());
     }
 
-    private Map<Character, Integer> fillMapValues (Map<Character, Integer> map, String path) throws IOException{
+    private Map<Character, Integer> fillMapValues(Map<Character, Integer> map, String path) throws IOException {
 
         StringBuilder builder = new StringBuilder();
         try (BufferedReader reader = Files.newBufferedReader(Path.of(path))) {
@@ -44,4 +65,16 @@ public class Parsing {
         return map;
     }
 
+    private List<Map.Entry<Character, Integer>> mapToList(Map<Character, Integer> map) {
+        Set<Map.Entry<Character, Integer>> entries = map.entrySet();
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>(entries);
+        list.sort((o1, o2) -> o2.getValue() - o1.getValue());
+
+//        @Override
+//        public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
+//            return o2.getValue()- o1.getValue();
+//        }
+
+        return list;
+    }
 }
